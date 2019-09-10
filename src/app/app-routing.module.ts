488d9029b6459d4bd450c893de06d41e6a0c64d0
promarg <elbouslamti.omar@gmail.com>
2019-09-10 16:42:06 +0200

    route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminSigninComponent } from './admin/admin-signin/admin-signin.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin/login', component: AdminSigninComponent }
+];
+
+//La liste des route que l'on retrouvera dans l'url et qui sera activé grace au routerLink
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
