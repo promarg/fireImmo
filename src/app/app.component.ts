3c9d0d7da9bae8004fb32b030e4599a66a7ef38f
promarg <elbouslamti.omar@gmail.com>
2019-09-10 16:07:18 +0200

    config firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'maSuperAgence';
+
+  constructor() {
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyB7pGNinxypns0Kep2iICLvEQvavTRXm5s",
+  authDomain: "masuperagence-25168.firebaseapp.com",
+  databaseURL: "https://masuperagence-25168.firebaseio.com",
+  projectId: "masuperagence-25168",
+  storageBucket: "",
+  messagingSenderId: "803911892237",
+  appId: "1:803911892237:web:c5cfd68fdea4ad80b88444"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  }
 }
